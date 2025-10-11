1c8369ec5baf69ffd4fe948d1eb0fa3c3331c335
Hadia Khalil <hadiyakhalil183@gmail.com>
2025-10-11 19:25:20 +0500

    first commit

--- src/views/FinalHome/index.tsx
+++ src/views/FinalHome/index.tsx
@@ -1,52 +1,45 @@
-import React from 'react'
+import React from "react";
+import Layout from "@/component/Layout";
+import CustomerSuccess from "./CustomerSuccess";
+import Excellence from "./Excellence";
+import FAQSection from "./FAQSection";
+import HeroSection from "./HeroSection";
+import KmcDifference from "./KmcDifference";
+import LocationSection from "./LocationSection";
+import PartnerSolution from "./PartnerSolution";
+import PremiumWorkspaces from "./PremiumWorkspaces";
+import PricingSection from "./PricingSection";
+import SectionPartnerCards from "./SectionPartnerCards";
+import SecurityCTA from "./SecurityCTA";
+import SecuritySection from "./SecuritySection";
+import TalentStrategy from "./TalentStrategy";
+import TeamSpaces from "./TeamSpaces";
+import TransformBusiness from "./TransformBusiness";
+import WhyKMC from "./WhyKMC";
 
-
-import CustomerSuccess from "./CustomerSuccess"
-import Excellence from "./Excellence"
-import FAQSection from "./FAQSection"
-import HeroSection from "./HeroSection"
-import KmcDifference from "./KmcDifference"
-import LocationSection from "./LocationSection"
-import PartnerSolution from "./PartnerSolution"
-import PremiumWorkspaces from "./PremiumWorkspaces"
-import PricingSection from "./PricingSection"
-import SectionPartnerCards from "./SectionPartnerCards"
-import SecurityCTA from "./SecurityCTA"
-import SecuritySection from "./SecuritySection"
-import TalentStrategy from "./TalentStrategy"
-import TeamSpaces from "./TeamSpaces"
-import TransformBusiness from "./TransformBusiness"
-import WhyKMC from "./WhyKMC"
-
-
-
-
-
-
-const FinalHome = ({ }) => {
+const FinalHome = ({}) => {
   return (
-  
-<>
-
-    <HeroSection/>
-     <PartnerSolution />
-     <PricingSection />
+    <>
+    <Layout>
+      <HeroSection />
+      <PartnerSolution />
+      <PricingSection />
       <WhyKMC />
       <KmcDifference />
-     <SectionPartnerCards />
-     <PremiumWorkspaces />
-     <LocationSection />
-     <TeamSpaces />
-     <SecuritySection />
-     <SecurityCTA />
-     <CustomerSuccess />
-     <TalentStrategy />
-     <Excellence />
-         <FAQSection />
-     <TransformBusiness />
-  </>
-      
-  )
-}
+      <SectionPartnerCards />
+      <PremiumWorkspaces />
+      <LocationSection />
+      <TeamSpaces />
+      <SecuritySection />
+      <SecurityCTA />
+      <CustomerSuccess />
+      <TalentStrategy />
+      <Excellence />
+      <FAQSection />
+      <TransformBusiness />
+      </Layout>
+    </>
+  );
+};
 
-export default FinalHome
+export default FinalHome;
